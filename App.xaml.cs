6987755fdd0c400739e86c95b30d27d287040b61
limoragam <33860860+limoragam@users.noreply.github.com>
2020-02-09 09:19:25 +0200

    Remove Window custom style

--- App.xaml.cs
+++ App.xaml.cs
@@ -7,10 +7,5 @@ namespace Daisy
     /// </summary>
     public partial class App : Application
     {
-        private void CloseClick(object sender, RoutedEventArgs e)
-        {
-            var window = (Window)((FrameworkElement)sender).TemplatedParent;
-            window.Close();
-        }
     }
 }
